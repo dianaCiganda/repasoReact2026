e1f4d669603a29e92d1df96df4a4b2aa2f90332d
julietaTabuyo <l.julietatabuyomail.com>
2026-03-16 22:29:44 -0300

    componentes creados

---
+++ EntregableReact/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Header from "./componentes/header"
+import Button from "./componentes/Button"
+import Cards from "./componentes/cards"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<Header/>
+<Button/>
+<Cards/>
+    </>
+  )
+}
+
+export default App
